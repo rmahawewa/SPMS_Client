8c3ee26a78fd5b20d46ede0a714e617a7aafe761
rmahawewa <rmahawewa@yahoo.com>
2025-10-06 17:59:22 +0530

    install dependencies, create routes

---
+++ src/components/Student.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Student = () => {
+	const [id, setId] = useState({});
+	const [nic, setNic] = useState("");
+	const [name, setName] = useState("");
+	const [dOfB, setDOfB] = useState("");
+	const [address, setAddress] = useState("");
+
+	return <div></div>;
+};
+
+export default Student;
